57f60fdf354fd4de02088d26eee2c167963d8cea
makstravm <maks7294@gmail.com>
2022-07-18 12:21:22 +0300

    added Layout and some meta tags

---
+++ components/Header.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+
+const Header = () => (
+  <>
+    <Head>
+      <title>Rick &amp; Morty | Info</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <meta
+        name="keywords"
+        content="Rick, Morty, Rick and Morty, movies"
+      ></meta>
+    </Head>
+  </>
+);
+
+export default Header;

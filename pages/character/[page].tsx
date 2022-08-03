66803959619ff923bd15b336adc5f2eeafd7e7ae
makstravm <maks7294@gmail.com>
2022-08-03 13:40:17 +0300

    changed request from REST API to GQL API in the character/[page].tsx

--- pages/character/[page].tsx
+++ pages/character/[page].tsx
@@ -4,6 +4,7 @@ import Layout from "components/Layout";
 import Character from "components/Character";
 
 import { IAllInfo, IPageCharacterProps } from "types/types";
+import { qgl } from "helpers/gql";
 
 export const getStaticPaths: GetStaticPaths = async () => {
   const response = await fetch(`${process.env.BASE_URL}/character`);
@@ -25,11 +26,30 @@ export const getStaticPaths: GetStaticPaths = async () => {
 };
 
 export const getStaticProps: GetStaticProps = async (context) => {
-  const response = await fetch(
-    `${process.env.BASE_URL}/character/${context.params?.page}`
+  const { data } = await qgl(
+    `query($id: ID!) {
+      character(id: $id) {
+        id
+        image
+        name
+        status
+        species
+        gender
+        created
+        origin {
+          id
+          name
+        }
+        location {
+          id
+          name
+        }
+        episode { id name episode}
+      }
+    }
+  `,
+    { id: context.params?.page }
   );
-
-  const data = await response.json();
 
   if (!data) {
     return {
@@ -38,9 +58,10 @@ export const getStaticProps: GetStaticProps = async (context) => {
   }
 
   return {
-    props: { character: data },
+    props: { character: data.character },
   };
 };
+
 const CharacterPage: NextPage<IPageCharacterProps> = ({ character }) => (
   <Layout title={character.name}>
     <Character character={character} />

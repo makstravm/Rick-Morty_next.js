9693d1d8bc57484c8a74705350c9d2380b4beb36
makstravm <maks7294@gmail.com>
2022-08-09 15:53:22 +0300

    added login api

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,60 @@
+import NextAuth from "next-auth";
+import CredentialProvider from "next-auth/providers/credentials";
+
+export default NextAuth({
+  providers: [
+    CredentialProvider({
+      name: "Credentials",
+      credentials: {
+        email: {
+          email: "Username",
+          type: "email",
+          placeholder: "example@email.com",
+        },
+        password: { label: "Password", type: "password" },
+      },
+      authorize: async (credentials) => {
+        const response = await fetch(`${process.env.API_URL}/login`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email: credentials?.email,
+            password: credentials?.password,
+          }),
+        });
+
+        const user = await response.json();
+
+        if (response.ok && user) {
+          return user;
+        }
+
+        return null;
+      },
+    }),
+  ],
+  callbacks: {
+    jwt: ({ token, user }) => {
+      if (user) {
+        token.accessToken = user.accessToken;
+      }
+
+      return token;
+    },
+
+    session: ({ session, token }) => {
+      session.accessToken = token.accessToken;
+
+      return session;
+    },
+  },
+
+  pages: {
+    signOut: "/auth/signout",
+    error: "/auth/error",
+    verifyRequest: "/auth/verify-request",
+    newUser: "/auth/new-user",
+  },
+});

22299718b80d1b5b2c52828a2491d51c1f527545
makstravm <maks7294@gmail.com>
2022-08-10 12:55:00 +0300

    added style validation

--- components/Login.tsx
+++ components/Login.tsx
@@ -1,7 +1,9 @@
 import Head from "next/head";
 import { yupResolver } from "@hookform/resolvers/yup";
-import { loginValidationSchema } from "helpers/schema/loginSchema";
 import { useForm } from "react-hook-form";
+
+import { loginValidationSchema } from "helpers/schema/loginSchema";
+
 import style from "../styles/Login.module.scss";
 
 interface MyForm {
@@ -13,15 +15,14 @@ export const LogIn = () => {
   const {
     register,
     handleSubmit,
-    formState: { errors },
+    formState: { errors, touchedFields },
   } = useForm<MyForm>({
-    mode: "onChange",
+    mode: "all",
+    defaultValues: {},
     resolver: yupResolver(loginValidationSchema),
   });
 
-  const submitForm = (data: MyForm) => {
-    return data;
-  };
+  const submitForm = (data: MyForm) => data;
 
   return (
     <>
@@ -42,13 +43,32 @@ export const LogIn = () => {
             <label htmlFor="chk" aria-hidden="true">
               Login
             </label>
-            <input {...register("email")} name="email" placeholder="email" />
-            <input
-              {...register("password")}
-              name="password"
-              type="password"
-              placeholder="password"
-            />
+            <div
+              className={
+                (errors.email && touchedFields.email && style.errors) || ""
+              }
+            >
+              <input {...register("email")} name="email" placeholder="email" />
+              {errors.email && touchedFields.email && (
+                <span>{errors.email.message}</span>
+              )}
+            </div>
+            <div
+              className={
+                (errors.password && touchedFields.password && style.errors) ||
+                ""
+              }
+            >
+              <input
+                {...register("password")}
+                name="password"
+                type="password"
+                placeholder="password"
+              />
+              {errors.password && touchedFields.password && (
+                <span>{errors.password?.message}</span>
+              )}
+            </div>
             <button type="submit">Log In</button>
           </form>
         </div>

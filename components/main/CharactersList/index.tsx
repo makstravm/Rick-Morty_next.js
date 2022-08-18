5c23db9024a80c8fee86802348e65bdbc1b4784b
makstravm <maks7294@gmail.com>
2022-08-18 14:36:07 +0300

    added FavoriteBtn to the CharacterList

--- components/main/CharactersList/index.tsx
+++ components/main/CharactersList/index.tsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 import { routesUrls } from "constants/routes";
 
 import { ICharactersListProps } from "./types";
+import FavoriteBtn from "components/FavoriteBtn";
 
 const { CHARACTER } = routesUrls;
 
@@ -14,12 +15,17 @@ const CharactersList: FC<ICharactersListProps> = ({ characters }) => (
       <div className="characters-list__item" key={character.id}>
         <Link href={`${CHARACTER}/${character.id}`}>
           <a>
-            <Image
-              src={character.image}
-              alt={character.name}
-              width={176}
-              height={176}
-            />
+            <div className="characters-list__box-img">
+              <Image
+                src={character.image}
+                alt={character.name}
+                width={176}
+                height={176}
+              />
+              <div className="characters-list__favorite">
+                <FavoriteBtn />
+              </div>
+            </div>
             <p>{character.name}</p>
           </a>
         </Link>

0d22f0232b208b8b56338f044e9344dd5936ca8a
makstravm <maks7294@gmail.com>
2022-08-22 18:25:34 +0300

    changed fallback in the Seasons Page

--- pages/seasons/[season].tsx
+++ pages/seasons/[season].tsx
@@ -42,7 +42,7 @@ export const getStaticProps: GetStaticProps = async (context) => {
     { filter: { episode: context.params?.season } }
   );
 
-  if (!results) {
+  if (!results.length) {
     return {
       notFound: true,
     };

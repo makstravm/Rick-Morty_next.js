64ac64d48109d910863115c75ee7cd3ecac09f19
makstravm <maks7294@gmail.com>
2022-09-02 17:31:51 +0300

    added head to the custom page 404, 500

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,5 +1,14 @@
 import PageError from "components/PageError";
+import Head from "next/head";
 
-const Custom404 = () => <PageError codeError="404" />;
+const Custom404 = () => (
+  <>
+    <Head>
+      <title>{` 404| Rick & Morty`}</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    </Head>
+    <PageError codeError="404" />;
+  </>
+);
 
 export default Custom404;
--- pages/500.tsx
+++ pages/500.tsx
@@ -1,5 +1,14 @@
 import PageError from "components/PageError";
+import Head from "next/head";
 
-const Custom500 = () => <PageError codeError="500" />;
+const Custom500 = () => (
+  <>
+    <Head>
+      <title>{` 404| Rick & Morty`}</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    </Head>
+    <PageError codeError="500" />;
+  </>
+);
 
 export default Custom500;
